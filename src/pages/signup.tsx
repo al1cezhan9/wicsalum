51565df73e972315d50664354f603075cb51ed60
Anna Bruhn <abru1500@icloud.com>
2026-01-26 20:24:53 -0500

    added cu/bc alum email access

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -14,7 +14,7 @@ const SignupPage: React.FC = () => {
     }
 
     // validate email domain
-    if (!email.endsWith('@columbia.edu') && !email.endsWith('@barnard.edu')) {
+    if (!email.endsWith('@columbia.edu') && !email.endsWith('@barnard.edu') && !email.endsWith('@alum.barnard.edu') && !email.endsWith('@caa.columbia.edu')) {
       setMessage('Please enter a valid @columbia.edu or @barnard.edu email.');
       return;
     }
